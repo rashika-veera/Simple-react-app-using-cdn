05f3a7c0b450bd54a3d497d3372c7d152dac2ed6
rashika-veera <rashikaveera@gmail.com>
2024-10-03 09:50:35 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,22 @@
+// const heading=React.createElement("h1",{id:"heading"},"Hello world from React !");
+const root=ReactDOM.createRoot(document.getElementById("root"));
+// console.log(heading,root)
+// root.render(heading);
+
+{/* <div id="parent">
+<div id="child1"><h1>I am a h1 tag</h1><h2>I am a h2 tag</h2></div>
+<div id="child2"><h1>I am a h1 tag</h1><h2>I am a h2 tag</h2></div>
+</div> */}
+
+const parent=React.createElement("div",{id:"parent"},[
+    React.createElement("div",{id:"child1"},[
+        React.createElement("h1",{id:"heading"},"I am a h1 tag"),
+        React.createElement("h2",{},"I am a h2 tag")
+    ]),
+    React.createElement("div",{id:"child2"},[
+        React.createElement("h1",{},"I am a h1 tag"),
+        React.createElement("h2",{},"I am a h2 tag")
+    ])
+])
+root.render(parent);
+console.log(parent);
